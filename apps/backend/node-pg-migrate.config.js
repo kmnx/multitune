5d8a308b3a4588e727bb085f43af1abdef3d7a48
kmnx <max.hampel@gmail.com>
2025-09-15 17:58:10 +0200

    i just love debugging tools instead of coding

--- apps/backend/node-pg-migrate.config.js
+++ apps/backend/node-pg-migrate.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   databaseUrl: 
-    `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+    (() => {
+      const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+      console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
+      return databaseUrl;
+    })(),
   migrationsTable: 'pgmigrations',
   dir: 'migrations',
 };
