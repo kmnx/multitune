fa8901a750509512bb032088de96eef998ce2299
kmnx <max.hampel@gmail.com>
2025-09-15 18:22:52 +0200

    no really

--- apps/backend/node-pg-migrate.config.js
+++ apps/backend/node-pg-migrate.config.js
@@ -1,9 +1,7 @@
-module.exports = () => {
-  const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-  console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
-  return {
-    migrationFolder: './migrations',
-    connectionString: databaseUrl,
-    direction: 'up',
-  };
+const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
+module.exports = {
+  migrationFolder: './migrations',
+  connectionString: databaseUrl,
+  direction: 'up',
 };
