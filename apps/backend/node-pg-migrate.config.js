43da6568a131919b67cfb4a2abcbe6f8a9d96cf4
kmnx <max.hampel@gmail.com>
2025-09-15 18:16:00 +0200

    copilot get bent

--- apps/backend/node-pg-migrate.config.js
+++ apps/backend/node-pg-migrate.config.js
@@ -1,16 +1,9 @@
-module.exports = {
-  databaseUrl: 
-    (() => {
-      const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-      console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
-      return databaseUrl;
-    })(),
-  module.exports = () => {
-    const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-    console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
-    return {
-      migrationFolder: './migrations',
-      connectionString: databaseUrl,
-      direction: 'up',
-    };
+module.exports = () => {
+  const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+  console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
+  return {
+    migrationFolder: './migrations',
+    connectionString: databaseUrl,
+    direction: 'up',
   };
+};
