73a71a8cb3da9d310644f48e91ff9bcde344fa80
kmnx <max.hampel@gmail.com>
2025-09-15 17:51:36 +0200

    cool tool

--- apps/backend/node-pg-migrate.config.js
+++ apps/backend/node-pg-migrate.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  databaseUrl: process.env.DATABASE_URL ||
+  databaseUrl: 
     `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   migrationsTable: 'pgmigrations',
   dir: 'migrations',
