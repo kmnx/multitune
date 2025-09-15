161a819ca31664b12b33f60027716b8acc3dbd5d
kmnx <max.hampel@gmail.com>
2025-09-15 18:08:31 +0200

    sure clanky

--- apps/backend/node-pg-migrate.config.js
+++ apps/backend/node-pg-migrate.config.js
@@ -5,6 +5,12 @@ module.exports = {
       console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
       return databaseUrl;
     })(),
-  migrationsTable: 'pgmigrations',
-  dir: 'migrations',
-};
+  module.exports = () => {
+    const databaseUrl = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+    console.log('node-pg-migrate DEBUG databaseUrl:', databaseUrl);
+    return {
+      migrationFolder: './migrations',
+      connectionString: databaseUrl,
+      direction: 'up',
+    };
+  };
